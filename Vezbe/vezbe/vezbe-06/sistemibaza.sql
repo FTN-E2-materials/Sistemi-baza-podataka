42c7dd311d71c2b4022b83dce042117d73561570
vlaksi <daksrapsody@gmail.com>
2021-04-22 20:37:52 +0200

    feat[lesson6]: second and third task

--- Vezbe/vezbe/vezbe-06/sistemibaza.sql
+++ Vezbe/vezbe/vezbe-06/sistemibaza.sql
@@ -50,3 +50,78 @@ VALUES (SEQUENCE_1.NEXTVAL, 'Jovan', 'Jovanovi', 10, 1000, 100, '01-JAN-87');
 UPDATE radnik
 set Ime = 'Peraaa'
 WHERE Mbr = 10;
+
+/*
+Formirati triger koji ?e, nad tabelom 
+Radnik, zabraniti bilo koji pokušaj modifikacije 
+vrednosti primarnog klju?a (mati?nog broja 
+radnika).
+*/
+CREATE OR REPLACE TRIGGER STOP_MODIFY_MBR
+    BEFORE
+    UPDATE OF Mbr
+    ON RADNIK
+    FOR EACH ROW WHEN (New.Mbr != Old.Mbr)
+    DECLARE
+        Zabrana_Menjanja_Mbr_EXC Exception;
+    BEGIN
+        RAISE Zabrana_Menjanja_Mbr_EXC;
+    EXCEPTION
+        WHEN Zabrana_Menjanja_Mbr_EXC THEN
+            Raise_Application_Error(-20000, 'Nije dozvoljeno menjanje obelezja MBR tabele radnik !');
+    END STOP_MODIFY_MBR;
+    
+UPDATE radnik
+set Mbr = 1000
+WHERE Mbr = 10;
+
+/*
+Formirati triger koji ?e, nad tabelom 
+Radnik, obezbediti da se prilikom unosa nove 
+torke, uvek zada vrednost mati?nog broja kao 
+prva slede?a vrednost iz kreiranog generatora 
+sekvence, bez obzira na to šta je korisnik zadao 
+za vrednost Mbr u klauzuli VALUES.
+*/
+
+CREATE OR REPLACE TRIGGER ZADAVANJE_NEXTVAL_MBRA
+    BEFORE
+    INSERT
+    ON Radnik
+    FOR EACH ROW
+    DECLARE
+        our_mbr Radnik.Mbr%TYPE := SEQUENCE_1.NEXTVAL;
+    BEGIN
+        :New.Mbr := our_mbr;
+    END ZADAVANJE_NEXTVAL_MBRA;
+
+
+INSERT 
+INTO radnik(Mbr, Ime, Prz, Sef, Plt, Pre, God) 
+VALUES (1000, 'Jovan', 'Jovanovi', 10, 1000, 100, '01-JAN-87');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
